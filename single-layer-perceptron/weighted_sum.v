f43187c0ac3afa38cbc9db27dd1c084e823fe40f
Shane Leonard <sw.leonard316@gmail.com>
2014-04-07 12:29:19 -0700

    Replace stub implementation with O(n) multi input adder in weighted_sum.v

--- single-layer-perceptron/weighted_sum.v
+++ single-layer-perceptron/weighted_sum.v
@@ -22,12 +22,30 @@ module weighted_sum(
     input [N-1:0] x,
     input [32*N-1:0] w,
     output [31:0] sum
-    );
+);
 
 parameter N = 8;
 
-// [TODO] Remove this stub implementation and replace it with a parameterized
-// adder tree
-assign sum = w[31:0];
+// [TODO] Improve this implementation: right now it takes N cycles to compute
+// the sum (one possibility: replace with an O(log(n)) tree)
+// [TODO] Account for overflow
+wire [32*N-1:0] intermediate_sums;
+
+always @(posedge clk) begin
+  intermediate_sums[31:0] <= w[31:0];
+end
+
+generate
+  genvar i;
+  for (i = 1; i < N; i = i + 1)
+  begin:weighted_sum
+    always @(posedge clk) begin
+      intermediate_sums[32*(i+1)-1:32*i] <= intermediate_sums[32*i-1:32*(i-1)] 
+                                            + w[32*(i+1)-1:32*i];
+    end
+  end
+endgenerate
+
+assign sum = intermediate_sums[32*N-1:32*(N-1)];
 
 endmodule
